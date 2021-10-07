2939a923f5b5b444a9e58bcac79043cda38f02fd
ArunArangan <arundark48@gmail.com>
2021-10-07 18:24:48 +0530

    Adding day5 task for anonymous function

--- B28WE/Day5/AnonymousFunctions(IIFE).js
+++ B28WE/Day5/AnonymousFunctions(IIFE).js
@@ -88,7 +88,7 @@ console.log(isPrime([2,3,4,5,6], 5));
  console.log(arry);
 })([2,3,4,5,6], 5);
 
-//Return all the palindromes in an array
+//e.Return all the palindromes in an array
 var findPalindrome = function (arr){
     let res = [];
   for ( var item of arr){
@@ -106,4 +106,75 @@ var findPalindrome = function (arr){
    return res;
 }
 
-console.log(findPalindrome([121, 131, 20 ,141 ,55, 56]))
+console.log(findPalindrome([121, 131, 20 ,141 ,55, 56]));
+
+
+//f.Return median of two sorted arrays of same size
+let median = function(arr1, arr2, n){
+  let i = 0;
+  let j = 0;
+  let count;
+  let m1 = -1;
+  let m2 = -1;
+
+  for(count = 0; count <= n; count++){
+
+    if(i === n){
+      m1 = m2;
+      m2 = arr2[0];
+      break;
+    }
+
+    if(j === n){
+      m1 = m2;
+      m2 = arr1[0];
+      break;
+    }
+
+    if( arr1[i] <= arr2[j]){
+       m1 = m2;
+       m2 = arr1[i];
+       i++;
+    }
+
+    else{
+      m1 = m2;
+      m2 = arr2[j];
+      j++;
+    }
+  }
+
+  return (m1+m2)/2;
+}
+let arr1 = [1, 12, 15, 26, 38];
+let arr2 = [2, 13, 17, 30, 45];
+var n1 = arr1.length;
+console.log(median(arr1, arr2, n1))
+
+//g.Remove duplicates from an array
+var removeDuplicate = function (arr , n){
+  let output = [];
+  for(let item of arr){
+    if(!output.includes(item))
+         output.push(item);
+  }
+  return output;
+}
+
+console.log(removeDuplicate([1, 2, 2, 3, 4, 4, 4, 5, 5]))
+
+
+//h.Rotate an array by k times
+let rotateArray = function(arr, k){
+    let temp = [];
+   for (let i = 0 ; i < k; i++)
+       temp.push(arr[i]);
+   
+   for (let j = 1; j <= k ; j++)
+       arr.shift();
+
+
+   return arr.concat(temp);
+}
+
+console.log(rotateArray([1, 12, 15, 26, 38],3));
