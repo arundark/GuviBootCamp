d09a4ca73ac9ab5dc1ee53367bfbc04ba9b01557
ArunArangan <arundark48@gmail.com>
2022-04-05 20:25:38 +0530

    Day4 task updated

--- B28WE/Day4/script.js
+++ B28WE/Day4/script.js
@@ -1,20 +1,20 @@
 //XML  HTTP REQUEST(xhr) (VIA API)
 // Step 01: create a XHR object
-var  request= new XMLHttpRequest();
+var request = new XMLHttpRequest();
 //Step 02:initatiate a connection(starting a connections)
 //true is used if your api doesnot want to affect the remaining  execution of your code
 
-request.open('GET','https://restcountries.eu/rest/v2/all',true);
+request.open("GET", "https://restcountries.com/v2/all", true);
 //step 03: sending a connection
 
 request.send();
-//step 04: once the data has been received successfully from server we need to extract the data to JSON 
+//step 04: once the data has been received successfully from server we need to extract the data to JSON
 
-//some conversion is needed here 
-request.onload=function(){
-var data=JSON.parse(request.response);
-console.log(data);
-for( var i = 0; i < data.length ; i ++){
-    console.log(data[i].name +' ' + data[i].capital)
-}
-}
+//some conversion is needed here
+request.onload = function () {
+  var data = JSON.parse(request.response);
+  console.log(data);
+  for (var i = 0; i < data.length; i++) {
+    console.log(data[i].name + " " + data[i].capital);
+  }
+};
